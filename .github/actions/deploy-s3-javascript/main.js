1d18b4e844f31dc8067c153808b15199e2321e8e
dev-abhirn <flx@qlik.com>
2026-01-14 11:04:07 +0530

    taking input

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,7 +1,12 @@
 const core = require("@actions/core");
+const github = require("@actions/github");
 
 function run() {
+  const bucket = core.getInput("bucket", { required: true });
+  const region = core.getInput("region", { required: true });
+  const dist = core.getInput("dist-folder", { required: true });
   core.notice("Hello from my custom JS action!");
+  core.notice(JSON.stringify({ bucket, region, dist }));
 }
 
 run();
